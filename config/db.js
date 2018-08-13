047902398c5133dd223bf6f9595a9c2071d5493a
ASHISH <ashish@ASHISHs-MacBook-Pro.local>
2018-08-13 23:09:59 +0530

    env vars change for heroku

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
-mongoose.connect('mongodb://'+process.env.BNP_DB_USER+':'+process.env.BNP_DB_PASS+'@'+process.env.BNP_DB_HOST+':'+process.env.BNP_DB_PORT+'/'+process.env.BNP_DB_NAME)
+mongoose.connect('mongodb://'+process.env.BNP_DB_USER+':'+process.env.BNP_DB_PASS+'@'+process.env.BNP_DB_HOST+':'+process.env.BNP_DB_PORT+'/'+process.env.BNP_DB_NAME,{ useNewUrlParser: true })
 .then(() => {
     mongoose.Promise = global.Promise
     console.log('conn establish')
